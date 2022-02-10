01433c33227ab32624eefb35040a2100b1261076
Nayeem29 <nayeembracu29@gmail.com>
2022-02-10 18:36:23 +0600

    finish

--- js/bank.js
+++ js/bank.js
@@ -2,21 +2,53 @@ const addMoney = document.querySelector('.deposit-money');
 const takeMoney = document.querySelector('.withdraw-money');
 
 let inputWithdrawAmount = document.querySelector('.withdraw-money').value;
+let totalTextAmount = document.querySelector('#amount');
+let totalAmount = parseFloat(totalTextAmount.innerText);
 
+
+// Deposit money: 
 addMoney.addEventListener('click', function() {
     let textDepositAmount = document.querySelector('.add-money');
-    const inputDepositAmount = textDepositAmount.value; //input value
+    const inputDepositAmount = parseFloat(textDepositAmount.value); //input deposit value
     console.log(inputDepositAmount);
     if (inputDepositAmount < 0) {
         alert('please provide valid amount');
     } else {
         let totalDeposit = document.querySelector('#deposit'); //total deposit value
-        let currentDeposit = totalDeposit.innerText;
+        let currentDeposit = parseFloat(totalDeposit.innerText);
         console.log(currentDeposit);
-        newTotalDeposit = parseFloat(inputDepositAmount) + parseFloat(currentDeposit);
+        newTotalDeposit = inputDepositAmount + currentDeposit;
         console.log(newTotalDeposit);
         totalDeposit.innerText = newTotalDeposit;
 
+        //total amount
+        totalAmount = totalAmount + inputDepositAmount;
+        console.log(totalAmount);
+        totalTextAmount.innerText = totalAmount;
+
     }
     textDepositAmount.value = '';
+})
+
+
+//withdraw money from account
+takeMoney.addEventListener('click', function() {
+    let textWithdrawAmount = document.querySelector('.take-money');
+    const inputWithdrawAmount = parseFloat(textWithdrawAmount.value);
+    if (inputWithdrawAmount < 0) {
+        alert('invalid amount request');
+    } else if (inputWithdrawAmount > totalAmount) {
+        alert('not available balance,please deposit first');
+    } else {
+        let withdrawnMoney = document.querySelector('#withdraw');
+        let withdrawnMoneyValue = parseFloat(withdrawnMoney.innerText);
+        let totalWithdrawnAmount = withdrawnMoneyValue + inputWithdrawAmount;
+        withdrawnMoney.innerText = totalWithdrawnAmount;
+
+        //total account balance:
+        totalAmount = totalAmount - inputWithdrawAmount;
+        console.log(totalAmount);
+        totalTextAmount.innerText = totalAmount;
+    }
+    textWithdrawAmount.value = '';
 })
